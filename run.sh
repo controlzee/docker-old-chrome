0c16463d48a1f83af9e70d63393c03b1f17489e8
Lily Skye <lily@controlzee.com>
2023-09-20 13:33:05 -0600

    oops I did the arg wrong

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 sleep 3 && ./open-browser.sh &
 
-docker run --rm -it --network host old-chrome-novnc chrome-linux/chrome --no-sandbox
+docker run --rm -it --net=host old-chrome-novnc chrome-linux/chrome --no-sandbox
